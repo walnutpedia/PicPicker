215b850f428850517e43e679e16d434221b5dfa6
walnutzhang <zerosnut@gmail.com>
2016-12-05 22:09:06 +0800

    basic function: done

--- src/content_script.js
+++ src/content_script.js
@@ -10,12 +10,12 @@
   var newImageElements = $('<div id="picPickerPickedImages"></div>');
 
   // get all img elements
-  var imgs = $('html img');
+  var imgs = getAllImgs();
 
   // get all elements with background
-    
+  var backgrounds = getAllBackgroundUrls();
 
-  // generate new image elements
+  //generate new image elements
   imgs.length && imgs.forEach(function(img){
     var imageBox = $('<div class="image"></div');
     var newImageElement = $('<img src="' + img.src + '">');
@@ -23,9 +23,39 @@
     newImageElements.append(imageBox);
   });
 
+  backgrounds.length && backgrounds.forEach(function(src){
+    var imageBox = $('<div class="image"></div');
+    var newImageElement = $('<img src="' + src + '">');
+    imageBox.append(newImageElement);
+    newImageElements.append(imageBox);
+  })
+
 
   picPickerSideBar.append(newImageElements);
 
   $('body').append(picPickerSideBar);
 
 })();
+
+function getAllImgs(){
+  var imgs = $('html img');
+  return imgs;
+}
+
+function getAllBackgroundUrls(){
+  var elements = $('*'),
+      length = elements.length,
+      backgroundUrlList = [],
+      regex = /"(.*?)"/g,
+      backgroundUrlList = [];
+  
+  for(var i = 0; i < length; i++){
+    var bgValue = elements.eq(i).css('background-image');
+    if(bgValue){
+      while(matches = regex.exec(bgValue)){
+        backgroundUrlList.push(matches[1]);
+      }
+    }
+  }
+  return backgroundUrlList;
+}
